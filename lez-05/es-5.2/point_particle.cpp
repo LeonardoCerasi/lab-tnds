b77a85f747d7525f264e125da51d6462fcb9e29d
Leonardo Cerasi <leonardo.cerasi@outlook.it>
2023-10-23 22:40:06 +0200

    Fixed errors

--- lez-05/es-5.2/point_particle.cpp
+++ lez-05/es-5.2/point_particle.cpp
@@ -12,18 +12,18 @@ point_particle::point_particle(double m, double q, const position& P) : particle
 
 vector_field point_particle::electric_field (const position& P) const
 {
-    double E_x = (K * q / pow(dist(P), 2)) * sin(P.get_coordinate('s', 2)) * cos(P.get_coordinate('s', 1));
-    double E_y = (K * q / pow(dist(P), 2)) * sin(P.get_coordinate('s', 2)) * sin(P.get_coordinate('s', 1));
-    double E_z = (K * q / pow(dist(P), 2)) * cos(P.get_coordinate('s', 2));
+    double E_x = (k * q / pow(dist(P), 2)) * sin(P.get_coordinate('s', 2)) * cos(P.get_coordinate('s', 1));
+    double E_y = (k * q / pow(dist(P), 2)) * sin(P.get_coordinate('s', 2)) * sin(P.get_coordinate('s', 1));
+    double E_z = (k * q / pow(dist(P), 2)) * cos(P.get_coordinate('s', 2));
 
     return vector_field(P, E_x, E_y, E_z);
 }
 
 vector_field point_particle::gravitational_field(const position &P) const
 {
-    double G_x = (G * m / pow(dist(P), 2)) * sin(P.get_coordinate('s', 2)) * cos(P.get_coordinate('s', 1));
-    double G_y = (G * m / pow(dist(P), 2)) * sin(P.get_coordinate('s', 2)) * sin(P.get_coordinate('s', 1));
-    double G_z = (G * m / pow(dist(P), 2)) * cos(P.get_coordinate('s', 2));
+    double G_x = (g * m / pow(dist(P), 2)) * sin(P.get_coordinate('s', 2)) * cos(P.get_coordinate('s', 1));
+    double G_y = (g * m / pow(dist(P), 2)) * sin(P.get_coordinate('s', 2)) * sin(P.get_coordinate('s', 1));
+    double G_z = (g * m / pow(dist(P), 2)) * cos(P.get_coordinate('s', 2));
 
     return vector_field(P, G_x, G_y, G_z);
 }
--- lez-05/es-5.2/point_particle.h
+++ lez-05/es-5.2/point_particle.h
@@ -7,8 +7,8 @@
 #include "particle.h"
 #include "vector_field.h"
 
-const double K = 8.9875517923e9;
-const double G = 6.67430e-11;
+const double k = 8.9875517923e9;
+const double g = 6.67430e-11;
 
 class point_particle : public particle, public position
 {
