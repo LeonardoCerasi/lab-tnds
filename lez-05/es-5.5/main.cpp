05ed125cae703324f6641a46bd124cc334ac481b
Leonardo Cerasi <leonardo.cerasi@outlook.it>
2023-10-30 17:48:30 +0100

    Finished esercizio 5.5 of lezione 05

--- lez-05/es-5.5/main.cpp
+++ lez-05/es-5.5/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include "TCanvas.h"
+#include "TAxis.h"
+#include "TGraph.h"
 
 #include "position.h"
 #include "particle.h"
@@ -24,23 +27,26 @@ int main(int argc, char** argv)
 
     spherical_body earth (m_t, r_t);
     std::vector<spherical_body> mountains;
-    position goce (r_t + d, 0., 0.);
-    std::vector<double> grav_field;
+    position goce('s', r_t + d, 0., M_PI_2);
+    std::vector<vector_field> grav_field;
 
     double earth_field = earth.gravitational_field(goce).mag();
     std::cout << "The ideal Earth's gravitational field at GOCE orbit is " << earth_field << " N/kg." << std::endl;
+
+    TGraph graph;
 
     try
     {
         for (int i{}; i < 100; i++)
         {
-            spherical_body mount(m, r, 's', r_t + r, i * phi_mount, M_PI / 2);
+            spherical_body mount(m, r, 's', r_t + r, M_PI_2 + i * phi_mount, M_PI_2);
             mountains.push_back(mount);
         }
 
         for (int i{}; i < std::floor(2 * M_PI / phi_goce); i++)
         {
-            goce.set_coordinate('s', 1, i * phi_goce);
+            double phi = goce.get_coordinate('s', 1);
+            goce.set_coordinate('s', 1, phi + phi_goce);
             
             vector_field G (goce);
             for (spherical_body mount : mountains)
@@ -48,7 +54,7 @@ int main(int argc, char** argv)
                 G += mount.gravitational_field(goce);
             }
 
-            grav_field.push_back(G.mag() / earth_field);
+            graph.SetPoint(i, G.get_coordinate('s', 1), G.mag() / earth_field);
         }
     }
     catch(const std::exception& e)
@@ -56,6 +62,15 @@ int main(int argc, char** argv)
         std::cerr << e.what() << std::endl;
         return -1;
     }
+
+    TCanvas canvas("canvas", "Gravitational field");
+    canvas.cd();
+    canvas.SetLogy();
+    graph.SetTitle("Gravitational field");
+    graph.Draw("ALP");
+    graph.GetXaxis()->SetTitle("Polar angle phi [rad]");
+    graph.GetYaxis()->SetTitle("delta_g / g");
+    canvas.SaveAs("graph.png");
 
     return 0;
 }
