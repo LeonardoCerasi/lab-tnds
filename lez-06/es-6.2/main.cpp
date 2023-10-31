ba0d675f3e7dc76759de59c019fc42ba03b134d5
Leonardo Cerasi <leonardo.cerasi@outlook.it>
2023-10-31 15:00:51 +0100

    6.2 - Added test functions

--- lez-06/es-6.2/main.cpp
+++ lez-06/es-6.2/main.cpp
@@ -4,13 +4,16 @@
 
 #include "functions.h"
 #include "bisection.h"
+#include "assert.h"
 
 int main()
 {
+    test::test_bisection();
+
     parabola p (3, 5, -2);
     bisection b;
     
-    std::cout << "Zero between 0 and 1: ";
+    std::cout << "\nZero between 0 and 1: ";
     std::cout << std::fixed << std::setprecision(-log10(b.get_precision())) << b.find_roots(0, 1, p) << std::endl;
 
     return 0;
