02595185b222d90bbdc0cee97afb4c72cbd47976
Leonardo Cerasi <leonardo.cerasi@outlook.it>
2023-10-31 15:06:32 +0100

    6.2 - Added test functions for bisection and secant method

--- lez-06/es-6.2/main.cpp
+++ lez-06/es-6.2/main.cpp
@@ -5,17 +5,20 @@
 #include "functions.h"
 #include "bisection.h"
 #include "secant.h"
+#include "assert.h"
 
 int main()
 {
+    test::test_bisection();
+
     int epsilon;
-    std::cout << "\nPrecision (max 1e-16): 1e-";
+    std::cout << "\nPrecision: 1e-";
     std::cin >> epsilon;
     parabola p (3, 5, -2);
     bisection b (pow(10, -epsilon));
     secant s(pow(10, -epsilon));
 
-    std::cout << "\nZero between 0 and 1: " <<std::endl;
+    std::cout << "\nZero between 0 and 1:\n" <<std::endl;
     std::cout << "(bisection)\t" << std::fixed << std::setprecision(-log10(b.get_precision())) << b.find_roots(0, 1, p) << std::endl << std::endl;
     std::cout << "(secant)\t" << std::fixed << std::setprecision(-log10(b.get_precision())) << s.find_roots(0, 1, p) << std::endl << std::endl;
 
