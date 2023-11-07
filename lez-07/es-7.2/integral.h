a4a5758437f70c258c906c8b2c952063f10c7145
Leonardo Cerasi <leonardo.cerasi@outlook.it>
2023-11-07 15:32:13 +0100

    7.* - Erased useless void constructor

--- lez-07/es-7.2/integral.h
+++ lez-07/es-7.2/integral.h
@@ -6,7 +6,6 @@ class numeric_integral
 {
     public:
 
-        numeric_integral() {}
         virtual ~numeric_integral() {}
 
         virtual double integral(double, double, std::function<double (double)>, double) = 0;
