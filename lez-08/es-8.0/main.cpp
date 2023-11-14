d4fcf26e9f59b7629dd4b574cab48cc01daccc78
Leonardo Cerasi <leonardo.cerasi@outlook.it>
2023-11-14 16:58:22 +0100

    8.0 - added files

---
+++ lez-08/es-8.0/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <array>
+
+#include "arr_op.h"
+#include "assert.h"
+
+int main()
+{
+    test::test_array();
+    std::cout << "Successful test" << std::endl;
+    return 0;
+}
